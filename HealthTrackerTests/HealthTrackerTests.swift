d2a6b51403ba364ce6166d9eea86d18d8df94468
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-05-25 12:53:58 +0100

    Initial Commit

---
+++ HealthTrackerTests/HealthTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerTests.swift
+//  HealthTrackerTests
+//
+//  Created by Aidan O'Brien on 25/05/2025.
+//
+
+import Testing
+@testable import HealthTracker
+
+struct HealthTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
