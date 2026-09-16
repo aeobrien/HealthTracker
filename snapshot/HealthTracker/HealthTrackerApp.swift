ab53673c7bbbbbb5f37a9e92f4afbd0c827503d2
Commit Guardian <guardian@localhost>
2026-09-16 01:28:54 +0100

    Commit Guardian snapshot aidans-mac-mini 2026-09-16 HealthTracker-6f300c22a30f

---
+++ snapshot/HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Aidan O'Brien on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
